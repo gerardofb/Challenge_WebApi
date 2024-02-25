a841a9331f55d9439d6e65f8a79307208d053e91
Gerardo Flores Barrie <gerardo.flores@impi.gob.mx>
2024-02-25 05:33:47 -0600

    Vista Materializada de Permisos de Usuario

--- Infrastructure/Migrations/20240225104821_Materialized-View.Designer.cs
+++ Infrastructure/Migrations/20240225104821_Materialized-View.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ChallengeContext))]
-    partial class ChallengeContextModelSnapshot : ModelSnapshot
+    [Migration("20240225104821_Materialized-View")]
+    partial class MaterializedView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/Migrations/20240225104821_Materialized-View.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class MaterializedView : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("EXEC('CREATE VIEW dbo.viewPermissions WITH SCHEMABINDING AS " +
+"SELECT EMP.ID, EMP.NAME AS USERNAME, EMP.LASTNAME AS LASTNAME, P.LASTUPDATED, PT.NAME, P.GUID " +
+"FROM DBO.EMPLOYEES EMP INNER JOIN DBO.EMPLOYEEPERMISSIONSEMPLOYEE EPERM ON EMP.ID = EPERM.EMPLOYEESID " +
+"INNER JOIN DBO.PERMISSIONS P ON P.ID = EPERM.PERMISSIONEMPLOYEESID INNER JOIN DBO.PERMISSIONSTYPES PT ON PT.ID = P.PERMISSIONTYPEID;');");
+            migrationBuilder.Sql("EXEC('SET NUMERIC_ROUNDABORT OFF; " +
+"SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT," +
+"QUOTED_IDENTIFIER, ANSI_NULLS ON;" +
+"CREATE UNIQUE CLUSTERED INDEX IDX_VPermissions " +
+"ON dbo.viewPermissions (LASTUPDATED, GUID);');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW dbo.viewPermissions;");
+        }
+    }
+}
